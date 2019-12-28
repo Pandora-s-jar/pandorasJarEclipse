093f05d1df4a69c48e040d728994eba5178ba0d7
SimoneMungari <mngsmn98c26d122a@studenti.unical.it>
2019-12-28 18:14:56 +0100

    Stupid file

---
+++ pandorasJarEclipse/src/controller/ChangeProfileDetails.java
@@ -0,0 +1,48 @@
+package controller;
+
+import model.User;
+import persistence.DBManager;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Paths;
+
+public class ChangeProfileDetails  extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        User u = (User) req.getSession().getAttribute("user");
+        String username = req.getParameter("inputUsername");
+        String email = req.getParameter("inputEmail");
+        String description = req.getParameter("inputDescription");
+        String password = req.getParameter("inputPassword");
+        String newFriend = req.getParameter("nameFriend");
+
+        //TODO: upload di una nuova immagine profilo
+
+        if(username != null && !username.equals(u.getUsername()))
+            u.setUsername(username);
+        if(email != null && !email.equals(u.getEmail()))
+            u.setEmail(email);
+        if(password != null && !password.equals(u.getPassword()))
+            u.setPassword(password);
+        if(description != null && !description.equals(u.getDescription()))
+            u.setDescription(description);
+
+        if(newFriend != null)
+        {
+           User friend = DBManager.getInstance().getUser(newFriend);
+           if(friend != null)
+           {
+               u.addFriend(friend);
+           }
+        }
+
+        resp.sendRedirect("profile");
+
+    }
+}

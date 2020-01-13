9e35f4695c3ee15c33e197c83915d8a2b0cbd2bb
luigimontaleone <mntlgu99b10d122d@studenti.unical.it>
2020-01-13 16:23:41 +0100

    no update

--- pandorasJarEclipse/src/controller/GeneralHelp.java
+++ pandorasJarEclipse/src/controller/GeneralHelp.java
@@ -33,8 +33,8 @@ public class GeneralHelp extends HttpServlet
 			String email = loggedUser.getEmail();
 			req.setAttribute("name", name);
 			req.setAttribute("email", email);
-			req.setAttribute("emailTo", to);
 		}
+		req.setAttribute("emailTo", to);
 		RequestDispatcher rd;
 		rd = req.getRequestDispatcher("header.jsp");
 		rd.include(req, resp);

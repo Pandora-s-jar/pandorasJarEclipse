6001f21dae74854c273dce8d99a1cbf0b54d7cf0
luigimontaleone <mntlgu99b10d122d@studenti.unical.it>
2020-01-14 20:46:42 +0100

    finita assistenza

--- pandorasJarEclipse/src/persistence/SoldGamesDAO.java
+++ pandorasJarEclipse/src/persistence/SoldGamesDAO.java
@@ -22,42 +22,55 @@ public class SoldGamesDAO
     public SoldGames getSoldGamesFromIdUser(int id)
     {
         Connection connection = DataSource.getInstance().getConnection();
-        String query = "SELECT EXTRACT(YEAR FROM date), game FROM public.purchase;";
-        try {
-            statement = connection.prepareStatement(query);
+        TreeMap<Integer,Integer> soldGPerYear = new TreeMap<Integer,Integer>();
+        TreeMap<Integer,Double> earnedMoneyPerYear = new TreeMap<Integer,Double>();
+        String queryYears = "SELECT EXTRACT(YEAR FROM date) FROM public.purchase;";
+        try
+        {
+            statement = connection.prepareStatement(queryYears);
+            ResultSet result = statement.executeQuery();
+            while(result.next())
+            {
+                soldGPerYear.put(Integer.valueOf(result.getString(1)), 0);
+                earnedMoneyPerYear.put(Integer.valueOf(result.getString(1)), 0.0);
+            }
+        } catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        String querySoldGames = "SELECT EXTRACT(YEAR FROM date), game FROM public.purchase;";
+        try
+        {
+            statement = connection.prepareStatement(querySoldGames);
             ResultSet result = statement.executeQuery();
             if(result.isClosed())
                 return null;
             SoldGames soldGames = new SoldGames();
-            TreeMap<Integer,Integer> soldGPerYear = new TreeMap<Integer,Integer>();
-            TreeMap<Integer,Double> earnedMoneyPerYear = new TreeMap<Integer,Double>();
             int idGame = 0;
             double currentPrice = 0;
             while(result.next())
             {
                 idGame = result.getInt("game");
-                System.out.println(idGame);
-
                 String currentYear = result.getString(1);
-                System.out.println(currentYear);
-                /*String queryGame = "SELECT * FROM public.game WHERE idGame = " + idGame + " AND developer = " + id + ";";
+                String queryGame = "SELECT * FROM public.game WHERE idGame = " + idGame + " AND developer = " + id + ";";
                 PreparedStatement statementGame = connection.prepareStatement(queryGame);
                 ResultSet resultGame = statementGame.executeQuery();
                 if(resultGame.next())
                 {
-                    soldGPerYear.put(currentYear, 1);//soldGPerYear.get(currentYear) + 1);
+                    soldGPerYear.put(Integer.valueOf(currentYear), soldGPerYear.get(Integer.valueOf(currentYear)) + 1);
                     currentPrice += resultGame.getDouble("price");
-                    earnedMoneyPerYear.put(currentYear, currentPrice);//earnedMoneyPerYear.get(currentYear) + currentPrice);
-                }*/
+                    earnedMoneyPerYear.put(Integer.valueOf(currentYear), earnedMoneyPerYear.get(Integer.valueOf(currentYear)) + currentPrice);
+                }
             }
-            //soldGames.setSoldGPerYear(soldGPerYear);
-            //soldGames.setEarnedMoneyPerYear(earnedMoneyPerYear);
+            soldGames.setSoldGPerYear(soldGPerYear);
+            soldGames.setEarnedMoneyPerYear(earnedMoneyPerYear);
             return soldGames;
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        finally{
+        finally
+        {
             DataSource.getInstance().closeConnection();
         }
         return null;

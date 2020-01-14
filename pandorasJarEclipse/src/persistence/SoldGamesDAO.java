f0147cd39e932ef54a76cbd9723c58ab9aca3c61
luigimontaleone <mntlgu99b10d122d@studenti.unical.it>
2020-01-14 20:48:15 +0100

    finita assistenza

--- pandorasJarEclipse/src/persistence/SoldGamesDAO.java
+++ pandorasJarEclipse/src/persistence/SoldGamesDAO.java
@@ -1,15 +1,11 @@
 package persistence;
 
-import model.Game;
 import model.SoldGames;
-import model.User;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.TreeMap;
 
 public class SoldGamesDAO
